b781ea4f20c42b3ca07d5df3ecb728f250463dff
Masashi Hasegawa <53898556+hiroshima820@users.noreply.github.com>
2022-02-20 17:59:10 +0900

    初回起動時にAPI Keyをテキストファイルに保存するよう実装

--- index.js
+++ index.js
@@ -2,25 +2,46 @@
 'use strict'
 
 const enquirer = require('enquirer')
+const fs = require('fs')
 const MovieJs = require("movie.js")
+const apiKeyPath = __dirname + '/apikey.txt'
 
 class Main {
+
+  constructor () {
+  if (fs.existsSync(apiKeyPath)) {
+        fs.readFile(apiKeyPath, (err, data) => {
+          this.apiKey = data
+          if (err) throw err;
+        });
+      } else {
+        this.apiKey = ''
+      }
+  }
+
   async run () {
-    const answer = await enquirer.prompt([{
-      type: 'input',
-      name: 'key',
-      message: 'Please enter your OMDb API key.\n:'
-    },{
+    if (!fs.existsSync(apiKeyPath)) {
+      const answer = await enquirer.prompt({
+        type: 'input',
+        name: 'key',
+        message: 'Please enter your OMDb API key.\n:'
+      })
+      fs.writeFile(apiKeyPath, answer.key, (error, result)=> {
+        if (error) console.log(error)
+      })
+    this.apiKey = answer.key
+    }
+    const answer = await enquirer.prompt({
       type: 'input',
       name: 'title',
       message:
         "Please enter the title of the movie you want to know the plot of in English.\nex.『となりのトトロ』=>'My Neighbor Totoro'\n:"
-    }])
+    })
     this.showMovie(answer)
   }
 
   showMovie(answer) {
-    new MovieJs(answer.key)
+    new MovieJs(this.apiKey)
       .getByTitle(answer.title, { plot: 'full' })
       .then((results) => {
         console.log('Title:', results.title)
